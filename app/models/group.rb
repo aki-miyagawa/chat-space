879185f2e81cc0a55f86529e3c99c8d53ea18d0a
aki-miyagawa <anyoneconsul@gmail.com>
2019-01-15 18:38:26 +0900

    Add#show_last_message

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,28 @@
 class Group < ApplicationRecord
   has_many :group_users
+  has_many :messages
   has_many :users, through: :group_users
   validates :name, presence: true
-  has_many :messages
+
+  def show_last_message
+    if (last_message = messages.last).present? #最新メッセージを変数last_messageに代入しつつメッセージが投稿されているかどうか場合分けをしている
+      last_message.content? ? last_message.content : '画像が投稿されています。' #三項演算子 [条件式 ? trueの時の値 : falseの時の値]
+
+    else
+      'まだメッセージはありません。'
+    end
+  end
 end
+
+# show_last_messageの違う書き方
+#   def show_last_message
+#     if (last_message = message.last).present?
+#       if last_message.content?
+#         last_message.content
+#       else
+#         '画像が投稿されています。'
+#       end
+#     else
+#       'まだメッセージはありません。'
+#     end
+#   end
