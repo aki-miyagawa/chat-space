1657e471da15d29b705b09285800a1a5d3367a04
aki-miyagawa <anyoneconsul@gmail.com>
2019-01-29 20:23:15 +0900

    Add to message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,27 +1,17 @@
 $(function(){
-
   function buildHTML(message){
-    var imagePresent = "";
-    if (message.image) {
-      imagePresent = `<img class="lower-message__image" src=${message.image}>`
-    }
-    var html = `<div class="messages">
-                  <div class="message">
-                    <div class="upper-message>
-                      <div class="upper-message__user-name">
-                        ${message.name}
-                      </div>
-                      <div class="upper-message__date">
-                        ${message.created_at}
-                      </div>
-                    </div>
-                    <div class="lower-message">
-                      <div class="lower-message__content">
-                      ${message.content}
-                      ${imagePresent}
-                      </div>
-                    </div>
-                  </div>
+    var image = (message.image === null) ? "" : `<img src="${message.image}" class="lower-message__image">`
+    var html = `<div class="upper-message" data-message-id="${message.id}">
+                  <p class="upper-message__user-name">
+                    ${message.user_name}
+                  </p>
+                  <p class="upper-message__date">
+                    ${message.created_at}
+                  </p>
+                  <p class="lower-message__image">
+                    ${message.content}
+                    ${image}
+                  </p>
                 </div>`
 
     return html;
@@ -30,7 +20,9 @@ $(function(){
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action')
+    var url  = $(this).attr('action');
+    var form = $(this);
+
     $.ajax({
       url:         url,
       type:        "POST",
@@ -39,16 +31,43 @@ $(function(){
       processData: false,
       contentType: false
     })
+
     .done(function(data){
       var html = buildHTML(data);
-      $('.messages').append(html)
-      $('#new_message')[0].reset()
+      $(".message").append(html)
+      form[0].reset()
+      $(".messages").animate({scrollTop: $(".messages")[0].scrollHeight}, 'fast');
     })
+
     .fail(function(){
-      alert('error')
+      alert('error');
     })
-    .always(() => {
-      $(".form__submit").removeAttr("disabled");
+
+    .always(function() {
+      $(".form__submit").prop('disabled', false)
     })
-  })
+  });
+
+  var interval = setInterval(function(){
+    if(window.location.href.match(/\/groups\/\d+\/messages/)){
+      var id = $('.upper-message:last').data('message-id')
+      $.ajax({
+        url: location.href,
+        type: 'GET',
+        dataType: 'json',
+        data: {id: id}
+      })
+      .done(function(messages){
+        messages.forEach(function(message){
+         $(".message").append(buildHTML(message));
+         });
+         $(".messages").animate({scrollTop: $(".messages")[0].scrollHeight}, 'fast');
+      })
+      .fail(function(data){
+        alert('自動更新に失敗しました')
+      })
+    }else{
+      clearInterval(interval);
+    }}, 5000);
+
 });
