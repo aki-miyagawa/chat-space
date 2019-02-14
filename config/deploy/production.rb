cd5f3728a0bd9374a6d21c98f579ff18b2a8331c
aki-miyagawa <anyoneconsul@gmail.com>
2019-02-14 20:44:26 +0900

    Edit production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,5 @@
+server '13.114.51.128', user: 'ec2-user', roles: %w{app db web}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
