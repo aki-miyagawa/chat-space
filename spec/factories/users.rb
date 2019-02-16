31441649f0e1b7a8496f35c8e1b30b0ad393c625
aki-miyagawa <anyoneconsul@gmail.com>
2019-02-16 13:09:56 +0900

    Fix factory user password

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :user do
     name { Faker::Name.last_name }
     email { Faker::Internet.free_email }
-    password { Faker::Internet.password(8) }
-    password password
-    password_confirmation password
+    password = Faker::Internet.password(8)
+    password { password }
+    password_confirmation { password }
   end
 end
